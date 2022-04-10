b3e608b9cfaf06d3a0ec5eb13fa421e2add9a25c
jiyangxu <xuyang1006@hotmail.com>
2022-04-10 20:40:26 +1000

    create and delete

--- Employee/Controllers/CompanyController.cs
+++ Employee/Controllers/CompanyController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Employee.Data;
 using Microsoft.AspNetCore.Mvc;
 
--- Employee/Controllers/EmployeeController.cs
+++ Employee/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Employee.Data;
 using Employee.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -44,6 +46,49 @@ namespace Employee.Controllers
                 return View(obj);
             
             _context.Employee.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET - Edit
+        public IActionResult Edit(Guid id)
+        {
+            var obj = _context.Employee.SingleOrDefault(x => x.Id == id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+        //POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Models.Employee obj)
+        {
+            if (!ModelState.IsValid)
+                return View(obj);
+
+            _context.Employee.Update(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET - Edit
+        public IActionResult Delete(Guid id)
+        {
+            var obj = _context.Employee.SingleOrDefault(x => x.Id == id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+        //POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(Models.Employee obj)
+        {
+            if (obj == null)
+                return NotFound();
+            _context.Employee.Remove(obj);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
